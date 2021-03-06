456b4002a72a7635ecf644b24ca082ae9cc8c7f5
dimonovdd <dimovde@yandex.ru>
2021-03-07 00:01:28 +0300

    init sample for https://github.com/xamarin/Xamarin.Forms/issues/9879

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue9879.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms.CustomAttributes;
+
+namespace Xamarin.Forms.Controls.Issues
+{
+	[Issue(IssueTracker.Github, 9879, "CollectionView footer/header bug on iOS", PlatformAffected.iOS)]
+	public partial class Issue9879 : TestContentPage
+	{
+		public Issue9879()
+		{
+#if APP
+			InitializeComponent();
+			TestCollection.ItemsSource = testItems;
+#endif
+		}
+
+		List<string> testItems = new List<string>()
+        {
+            "item 1",
+            "item 2",
+            "item 3",
+            "item 4",
+            "item 5",
+            "item 6",
+            "item 7",
+            "item 8",
+            "item 9",
+            "item 10",
+		};
+
+        protected override void Init()
+		{
+			
+		}
+	}
+}
--- Xamarin.Forms.Platform.iOS/CollectionView/CollectionViewRenderer.cs
+++ Xamarin.Forms.Platform.iOS/CollectionView/CollectionViewRenderer.cs
@@ -4,5 +4,49 @@
 	{
 		[Internals.Preserve(Conditional = true)]
 		public CollectionViewRenderer() { }
+
+		KeyboardInsetTracker _insetTracker;
+		bool _disposed;
+
+		public override void LayoutSubviews()
+		{
+			_insetTracker?.OnLayoutSubviews();
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<GroupableItemsView> e)
+		{
+			//if (e.NewElement != null)
+			//{
+			//	if (Control == null)
+			//	{
+			//		_insetTracker = new KeyboardInsetTracker(_tableViewController.TableView, () => Control.Window, insets => Control.ContentInset = Control.ScrollIndicatorInsets = insets, point =>
+			//		{
+			//			var offset = Control.ContentOffset;
+			//			offset.Y += point.Y;
+			//			Control.SetContentOffset(offset, true);
+			//		}, this);
+			//	}
+			//}
+			base.OnElementChanged(e);
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (_disposed)
+				return;
+
+			if (disposing)
+			{
+				if (_insetTracker != null)
+				{
+					_insetTracker.Dispose();
+					_insetTracker = null;
+				}
+			}
+
+			_disposed = true;
+
+			base.Dispose(disposing);
+		}
 	}
 }
